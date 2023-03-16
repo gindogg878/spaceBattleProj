7bd4df60ad71143c4a2835c24662a2598fd24cc5
Gino Torres Jr <gindogg878@icloud.com>
2023-03-16 16:44:14 -0500

    first try at enemy counter attack

--- script.js
+++ script.js
@@ -17,30 +17,55 @@ class Myship extends Ships {
   attackAl = function (alienHull) {
     if (alienHull > this.firepower) {
       alienHull = alienHull - this.firepower;
-    } else if (alienHull < this.firepower) {
-      console.log("you've destroyed an alien ship!!!");
-    }
+      //   console.log(alienHull);
+      Alien.attackShip();
+    } else if (alienHull > 0) {
+      while (alienhull) {
+        alienHull - this.firepower;
+      }
+    } else alienHull < this.firepower;
+    console.log("you've destroyed an alien ship!!!");
   };
 }
 class Alien extends Ships {
   constructor() {
     super();
-    this.hull = Math.random() * 10;
-    this.firepower = Math.random() * 10;
-    this.accuracy = Math.random();
+    this.hull = Math.floor(Math.random() * 6) + 3;
+    // this.hull = Math.random();
+    this.firepower = Math.floor(Math.random() * 4) + 2;
+    this.accuracy = Math.random() + 0.6;
   }
-  accuracyVal = function (accuracy) {
-    if (Math.random() < this.accuracy) console.log(`You've been hit!`);
-  };
 
-  hullVal = function (hull) {
-    if (Math.random() < hull) {
-      hull;
+  attackShip(myshipHull) {
+    if (myshipHull > this.firepower) {
+      myshipHull = myshipHull - this.firepower;
+      console.log(`Enemy Attacked and caused ${this.firepower} damage`);
+      return;
+    } else if (myshipHull < this.firepower) {
+      console.log("The enemy has defeated you");
     }
-  };
+  }
+  //   accuracyVal = function (accuracy) {
+  //     if (Math.random() < this.accuracy) console.log(`You've been hit!`);
+  //   };
+
+  //   hullVal = function (hull) {
+  //     if (Math.random() < hull) {
+  //       hull;
+  //     }
+  //   };
 }
 
-// const answer = window.prompt("Attack?");
-// console.log(answer);
+const ussPerScholas = new Myship();
+// console.log(ussPerScholas);
+const sith = new Alien();
+console.log(sith);
+const answer = window.prompt("Attack?");
+console.log(answer);
+if (answer === "yes") {
+  ussPerScholas.attackAl(sith.hull);
+}
+console.log(sith);
+
 // let yourAttack = window.prompt("You destroyed a ship!!! Continue: yes or no?");
 // console.log(yourAttack);
