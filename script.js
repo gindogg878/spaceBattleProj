cfb585bd3f7179e0e79ffd40ce14746dad425432
Gino Torres Jr <gindogg878@icloud.com>
2023-03-16 15:10:21 -0500

    first try at attack function for myShip

--- script.js
+++ script.js
@@ -1,10 +1,46 @@
 class Ships {
-  constructor(hull, firepower, accuracy) {
+  constructor() {
     this.hull = 0;
     this.firepower = 0;
     this.accuracy = 0;
   }
-  accuracyVal = function (accuracy) {
-    if (Math.random() < alien.accuracy) console.log(`You've been hit!`);
+}
+
+class Myship extends Ships {
+  constructor() {
+    super();
+    this.hull = 20;
+    this.firepower = 5;
+    this.accuracy = 0.7;
+  }
+
+  attackAl = function (alienHull) {
+    if (alienHull > this.firepower) {
+      alienHull = alienHull - this.firepower;
+    } else if (alienHull < this.firepower) {
+      console.log("you've destroyed an alien ship!!!");
+    }
   };
 }
+class Alien extends Ships {
+  constructor() {
+    super();
+    this.hull = Math.random() * 10;
+    this.firepower = Math.random() * 10;
+    this.accuracy = Math.random();
+  }
+  accuracyVal = function (accuracy) {
+    if (Math.random() < this.accuracy) console.log(`You've been hit!`);
+  };
+
+  hullVal = function (hull) {
+    if (Math.random() < hull) {
+      hull;
+    }
+  };
+}
+
+// const answer = window.prompt("Attack?");
+// console.log(answer);
+// let yourAttack = window.prompt("You destroyed a ship!!! Continue: yes or no?");
+// console.log(yourAttack);
